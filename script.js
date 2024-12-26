6c13891e3d353d99387ae23e86f4db8eb90c6033
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2024-12-26 21:37:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function shortcut(s1, s2) {
-  // your code here
-	
- return s1 + s2;
-
+  if (s1 === "" || s2 === "") {
+    return "";
+  } else {
+    return s1.charAt(0) + s2.charAt(0);
+  }
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
